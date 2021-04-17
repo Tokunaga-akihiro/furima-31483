c606bf68dcdd50f34cd9399a1504e9f42469c224
Tokunaga-akihiro <74903128+Tokunaga-akihiro@users.noreply.github.com>
2021-04-17 22:04:52 +0900

    修正漏れの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,9 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :birthday  
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters." }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters." }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters." }
-    validates :first_name_kana, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters." }
+    validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters." } do
+    first_name
+    last_name
+    end
   end
-end
